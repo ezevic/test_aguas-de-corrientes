d031e3171d6d2454a8f644b2a2086794732ce88b
ezevic <eze_vic_98@hotmail.com>
2022-10-25 16:30:54 -0300

    modelo Palindromo agregado al panel de admin de django

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
+from atexit import register
 from django.contrib import admin
 
+from .models import Palindromo
+
 # Register your models here.
+
+
+@admin.register(Palindromo)
+class AdminPalindromo(admin.ModelAdmin):
+    list_display = ('palabra', 'check_palindromo')
