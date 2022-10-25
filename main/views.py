92cdece244718cdc17e0e4cdb951fabe512e3622
ezevic <eze_vic_98@hotmail.com>
2022-10-25 16:31:39 -0300

    agergada la vista index

--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
+from multiprocessing import context
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    context={}
+
+    return render(request, 'index.html', context=context)
