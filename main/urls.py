9c7e69bcbaeabf789455d79ddba21be26ed1691c
ezevic <eze_vic_98@hotmail.com>
2022-10-26 00:06:33 -0300

    agregada la url para la vista de lista

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import index
+from .views import index, PalabrasListView
 
 urlpatterns = [
     path('', index, name='index'),
+    path('lista_palabra', PalabrasListView.as_view(), name='lista')
 ]
 
