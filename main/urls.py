43404eec9caeb82e5c865803113004afae84ad3f
ezevic <eze_vic_98@hotmail.com>
2022-10-26 10:03:41 -0300

    agregadas las urls para la detailview de las palabras, para registrar usuarios e iniciar sesion

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
-from .views import index, PalabrasListView, palabra_detail_view
+from .views import index, PalabrasListView, palabra_detail_view, register, login_user
 
 urlpatterns = [
     path('', index, name='index'),
-    path('lista_palabra', PalabrasListView.as_view(), name='lista'),
+    path('lista_palabra/', login_required(PalabrasListView.as_view()), name='lista'),
     path('palabra/<int:pk>', palabra_detail_view, name='palabra-detail'),
+    path("register/", register, name="register"),
+    path("login/", login_user, name="login_user"),
 ]
 
