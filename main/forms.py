e7f9705d58591a90c6e0e873d0cc6003f3470e7d
ezevic <eze_vic_98@hotmail.com>
2022-10-26 10:02:54 -0300

    form para registrar usuarios agregado

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,6 @@
-from dataclasses import fields
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import Palindromo
 
@@ -7,16 +8,23 @@ from .models import Palindromo
 # class to define the form
 class PalindromoForm(forms.ModelForm):
 
-
     class Meta:
-        model=Palindromo
+        model = Palindromo
         fields = ['palabra']
 
-        labels={
+        labels = {
             'palabra': ''
         }
 
         widgets = {
 
-            'palabra': forms.TextInput(attrs={'class': 'form-control-lg w-100','placeholder': 'Ingresa una palabra o frase' , 'required': True})
+            'palabra': forms.TextInput(attrs={'class': 'form-control-lg w-100', 'placeholder': 'Ingresa una palabra o frase', 'required': True})
         }
+
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
