5092adab3add772112f79a563d2ec84e38a76e35
ezevic <eze_vic_98@hotmail.com>
2022-10-26 06:23:21 -0300

    agregada url para 'palabra-detail'

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index, PalabrasListView
+from .views import index, PalabrasListView, palabra_detail_view
 
 urlpatterns = [
     path('', index, name='index'),
-    path('lista_palabra', PalabrasListView.as_view(), name='lista')
+    path('lista_palabra', PalabrasListView.as_view(), name='lista'),
+    path('palabra/<int:pk>', palabra_detail_view, name='palabra-detail'),
 ]
 
